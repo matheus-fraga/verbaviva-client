8cd67f4c9811f8e9368d3109e32b818f1ea0b982
andradev101 <santos.andradetb@gmail.com>
2024-06-13 01:39:57 -0300

    project page + list visualization (unstyled) + vote backend feedback

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,10 +1,29 @@
 <script setup>
-    import Navbar from '../components/Navbar.vue'
+  import { ref, onMounted, onUnmounted, onBeforeMount } from 'vue';
+  import Navbar from '../components/Navbar.vue'
+  import ProjectList from '../components/ProjectList.vue'
 </script>
 
 <template>
-  <main>
     <Navbar/>
     <p>this is the project view page</p>
-  </main>
+    <p class="red">DEV: para peformar votos:</p>
+    <p class="red">Em components/ProjectVote.vue > prepareRequest()</p>
+    <p class="red">Modifique o "projetoId" e "usuarioId"</p>
+    <ProjectList/>  
 </template>
+
+<style scoped>
+    p {
+        margin: 0;
+    }
+    .red {
+        color: red;
+    }
+    .green {
+        color: rgb(67, 168, 13);
+    }
+    .spacer {
+        margin: 20px 0;
+    }
+</style>
