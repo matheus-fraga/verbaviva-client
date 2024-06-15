9c8413b80694a210acee3b2abbe20166437f9399
andradev101 <santos.andradetb@gmail.com>
2024-06-15 11:07:47 -0300

    implemented login component

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { NButton, NCard, NSpace, NSwitch, NTag, NInput, NDatePicker, NDivider } from 'naive-ui';
+import { onMounted, ref } from 'vue';
+const emit = defineEmits(["emitLoginComponentClose"]);
+
+
+let dividerTextPlacement = ref("center");
+onMounted (()=>{
+    setStuffBasedOnScreenSize();
+})
+
+function setStuffBasedOnScreenSize() {
+    let width = window.innerWidth;
+  if (width <= 900) {
+    dividerTextPlacement.value = "center";
+  } else {
+    dividerTextPlacement.value = "left";
+  }
+}
+function emitLoginComponentClose() {
+    emit("emitLoginComponentClose")
+}
+</script>
+
+<template>
+    <div>
+        <n-card title="Login" hoverable closable size="small" @close="emitLoginComponentClose">
+            <n-space vertical>
+                <n-input size="tiny" placeholder="CPF" />
+                <n-date-picker size="tiny" type="date" />
+                <n-button size="tiny" type="primary">Login</n-button>
+            </n-space>
+        </n-card>
+    </div>
+</template>
