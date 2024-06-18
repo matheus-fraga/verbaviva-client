7c16e675332cc2a345a542e7fefe6ccae715b8cb
andradev101 <santos.andradetb@gmail.com>
2024-06-18 18:49:25 -0300

    neonavbar now displays the login component on click

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -18,12 +18,12 @@ function emitUserStateGlobalEvent(userPublicInfoPayload) {
   eventBus.emit('update-login-info-global-event', { data: userPublicInfoPayload });
 }
 
-onMounted (()=>{
-    setStuffBasedOnScreenSize();
+onMounted(() => {
+  setStuffBasedOnScreenSize();
 })
 
 function setStuffBasedOnScreenSize() {
-    let width = window.innerWidth;
+  let width = window.innerWidth;
   if (width <= 900) {
     dividerTextPlacement.value = "center";
   } else {
@@ -31,95 +31,90 @@ function setStuffBasedOnScreenSize() {
   }
 }
 function emitLoginComponentClose() {
-    emit("emitLoginComponentClose")
+  emit("emitLoginComponentClose")
 }
 
 function fazLogin() {
   console.log(cpf.value)
   console.log(dataNascimento.value)
 
+  if (!cpf.value || !dataNascimento.value) {
+    console.log("CPF e data de nascimento são obrigatórios");
+  }
 
+  const formulario = {
+    cpf: cpf.value,
+    dataNascimento: timestampToDate(dataNascimento.value)
+  };
 
-    if (!cpf.value || !dataNascimento.value) {
-        console.log("CPF e data de nascimento são obrigatórios");
-    }
-
-    const formulario = {
-        cpf: cpf.value,
-        dataNascimento: timestampToDate(dataNascimento.value)
-    };
-
-    fetch('http://localhost:8080/usuarios/auth', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formulario)
-    })
+  fetch('http://localhost:8080/usuarios/auth', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formulario)
+  })
     .then(response => response.json())
     .then(data => {
-        
-        if (data.authenticated) {
-            console.log("usuário localizado");
-            user.signIn(data);
-            showLogoutButton.value = true;
-            showLoginButton.value = false;
-            emit('updateLoginInfo')
-            emitUserStateGlobalEvent(data);
-        } else {
-            console.log("usuário não localizado");
-        }
+
+      if (data.authenticated) {
+        console.log("usuário localizado");
+        user.signIn(data);
+        emit('updateLoginInfo')
+        emitUserStateGlobalEvent(data);
+      } else {
+        console.log("usuário não localizado");
+      }
     })
     .catch(error => {
-        console.log("Erro na autenticação: ", error);
+      console.log("Erro na autenticação: ", error);
     });
 }
 
- function fazLogout(){
+function fazLogout() {
   user.signOut();
-  showLoginButton.value = true;
-  showLogoutButton.value = false;
   emit('updateLoginInfo');
   emitUserStateGlobalEvent({})
 }
 
 function handleUpdateLoginInfoGlobalEvent(userData) {
-    if(Object.keys(userData.data).length === 0) {
-        showLoginButton.value = true;
-        return;
-    }
-    // console.log(userData);
-    showLoginButton.value = false;
+  if (Object.keys(userData.data).length === 0) {
+    showLoginButton.value = true;
+    showLogoutButton.value = false;
+    return;
+  }
+  showLoginButton.value = false;
+  showLogoutButton.value = true;
 }
 
 function timestampToDate(timestamp) {
-    // Create a new Date object with the given timestamp
-    const date = new Date(timestamp);
+  // Create a new Date object with the given timestamp
+  const date = new Date(timestamp);
 
-    // Extract the year, month, and day from the Date object
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so we add 1
-    const day = String(date.getDate()).padStart(2, '0');
+  // Extract the year, month, and day from the Date object
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so we add 1
+  const day = String(date.getDate()).padStart(2, '0');
 
-    // Format the date as YYYY-MM-DD
-    const formattedDate = `${year}-${month}-${day}`;
+  // Format the date as YYYY-MM-DD
+  const formattedDate = `${year}-${month}-${day}`;
 
-    return formattedDate;
+  return formattedDate;
 }
 
 </script>
 
 <template>
-    <div>
-      <n-card title="Login" hoverable closable size="small" @close="emitLoginComponentClose">
-        <n-space vertical>
-          <n-input v-model:value="cpf" size="tiny" placeholder="CPF" />
-          <n-date-picker v-model:value="dataNascimento" size="tiny" type="date" />
-          <n-space>
-            <n-button v-show="showLoginButton" size="tiny" type="primary" @click="fazLogin">Login</n-button>
-            <n-button v-show="showLogoutButton" size="tiny" type="secondary" @click="fazLogout">Logout</n-button>
-          </n-space>
+  <div>
+    <n-card title="Login" hoverable size="small" @close="emitLoginComponentClose">
+      <n-space vertical>
+        <n-input v-model:value="cpf" size="tiny" placeholder="CPF" />
+        <n-date-picker v-model:value="dataNascimento" size="tiny" type="date" />
+        <n-space>
+          <n-button v-show="showLoginButton" size="tiny" type="primary" @click="fazLogin">Login</n-button>
+          <n-button v-show="showLogoutButton" size="tiny" type="secondary" @click="fazLogout">Logout</n-button>
         </n-space>
-      </n-card>
-    </div>
-  </template>
+      </n-space>
+    </n-card>
+  </div>
+</template>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,31 +1,32 @@
 <script setup>
-  import { ref, onMounted, onUnmounted, onBeforeMount } from 'vue';
-  import Navbar from '../components/Navbar.vue'
-  import NeoNavBar from '../components/NeoNavBar.vue'
-  import ProjectList from '../components/ProjectList.vue'
-  import { NButton, useNotification, NNotificationProvider } from 'naive-ui'
+import { ref, onMounted, onUnmounted, onBeforeMount } from 'vue';
+import Navbar from '../components/Navbar.vue'
+import NeoNavBar from '../components/NeoNavBar.vue'
+import ProjectList from '../components/ProjectList.vue'
+import { NButton, useNotification, NNotificationProvider } from 'naive-ui'
 </script>
 
 <template>
     <NeoNavBar />
-    <p class="red">para que o botao criar projeto apareca, faca o login e atualize a pagina (por enquanto)</p>
     <n-notification-provider>
-    <ProjectList/>
+        <ProjectList />
     </n-notification-provider>
-
 </template>
 
 <style scoped>
-    p {
-        margin: 0;
-    }
-    .red {
-        color: red;
-    }
-    .green {
-        color: rgb(67, 168, 13);
-    }
-    .spacer {
-        margin: 20px 0;
-    }
+p {
+    margin: 0;
+}
+
+.red {
+    color: red;
+}
+
+.green {
+    color: rgb(67, 168, 13);
+}
+
+.spacer {
+    margin: 20px 0;
+}
 </style>
