2926a82e2fdfd56d38f17b4573c9e3e21772847a
danielfmf25 <129324021+danielfmf25@users.noreply.github.com>
2024-06-20 08:33:28 -0300

    Add description of app on home
    Update style in "Sobre nós" page

--- src/views/ParticipatoryBudgetingView.vue
+++ src/views/ParticipatoryBudgetingView.vue
@@ -1,42 +1,72 @@
 <script setup>
-    import NeoNavBar from '../components/NeoNavBar.vue'
+import {ref} from 'vue'
+import NeoNavBar from '../components/NeoNavBar.vue'
+import {NGradientText} from 'naive-ui'
+
+let gradientColor = ref("linear-gradient(90deg, rgba(120,10,10,1) 12%, rgba(172,55,55,1) 50%, rgba(120,10,10,1) 76%)");
 </script>
 
 <template>
-  <main>
-    <NeoNavBar/>
-    <div class="container my-5">
-    <div class="jumbotron text-center">
-      <h1 class="display-4">Verba Viva!</h1>
-      <p class="lead">Sua voz, seu poder!</p>
+  <main class="text-center">
+    <NeoNavBar />
+
+    <n-gradient-text :gradient="gradientColor">
+      <h1>Verba Viva: Uma Jornada para a Gestão Participativa do Município</h1>
+    </n-gradient-text>
+
+    <p class="lead">
+      Sabe aquela fase do mês em que a conta aperta? É preciso comprar vários itens, mas o dinheiro que sobra não dá para tudo. O que fazer? Simples: priorizamos o essencial e deixamos as compras menos urgentes para depois.
+      Imagine se, em vez da sua casa, estivéssemos falando de um município. Nesse caso, você, cidadão, teria a responsabilidade de decidir como investir o dinheiro público, a verba municipal, naquele momento. Agora, imagine um lugar onde você e outras pessoas podem ver e elencar os problemas da região onde moram e promover a mudança. PRONTO!
+      <n-gradient-text :gradient="gradientColor">Bem-vindo ao VERBA VIVA!</n-gradient-text>
+    </p>
+
+    <p class="lead">
+      Você sabia que em 2024, a Prefeitura de Belo Horizonte destina R$73 milhões exclusivamente para obras do Orçamento Participativo (OP)? Sim, sua voz, seu poder!
+    </p>
+
+    <NGradientText :gradient="gradientColor">
+      <h2 class="my-4">Como funciona o Verba Viva?</h2>
+    </NGradientText>
+
+    <p class="lead">
+      O OP te coloca no comando das decisões! Você escolhe quais melhorias serão feitas no seu município, garantindo que os recursos públicos sejam investidos em projetos que realmente impactam sua vida.
+    </p>
+
+    <p class="lead">
+      Cansado de ver o dinheiro público mal utilizado? O OP é a sua chance de mudar isso! Através dele, você pode:
+    </p>
+
+    <ul class="list-group list-group-flush mb-4">
+      <NGradientText :gradient="black">
+        <li class="list-group-item">Propor projetos: Apresente ideias para obras e serviços que beneficiem sua comunidade.</li>
+        <li class="list-group-item">Votar nas prioridades: Decida quais projetos receberão os investimentos.</li>
+        <li class="list-group-item">Acompanhar a execução: Monitore de perto o andamento das obras e cobre resultados.</li>
+      </NGradientText>
+    </ul>
+
+    <NGradientText :gradient="gradientColor">
+      <p class="text-justify">Junte-se ao Verba Viva e faça a diferença!</p>
+    </NGradientText>
+
+    <div class="section-content">
+      <NGradientText :gradient="gradientColor">
+        <h2 class="my-4">Vantagens do Verba Viva</h2>
+      </NGradientText>
+
+      <p class="text-justify">
+        O Verba Viva é um programa inovador que simplifica o OP:
+      </p>
+
+      <p class="text-justify">Acesso facilitado: Utilize de uma interface simples e amigável para compreender aquilo que seria complicado.</p>
+
+      <p class="text-justify">Informações na palma da mão: Tenha acesso a projetos de todas as regionais de BH e entenda para onde irão após propostos.</p>
+
+      <NGradientText :gradient="gradientColor">
+        <p class="text-center">Seu bairro, suas decisões!</p>
+        <p class="text-center">Não perca essa oportunidade! Participe do OP e ajude a construir uma Belo Horizonte mais justa, próspera e com a sua cara!</p>
+      </NGradientText>
     </div>
-    <div class="row">
-      <div class="col-md-12">
-        <p class="text-justify">Sabe aquela fase do mês em que a conta aperta? É preciso comprar vários itens, mas o dinheiro que sobra não dá para tudo. O que fazer? Simples: priorizamos o essencial e deixamos as compras menos urgentes para depois.</p>
-        <p class="text-justify">Imagine se, em vez da sua casa, estivéssemos falando de um município. Nesse caso, você, cidadão, teria a responsabilidade de decidir como investir o dinheiro público, a verba municipal, naquele momento. Agora, imagine um lugar onde você e outras pessoas podem ver e elencar os problemas da região onde moram e promover a mudança. <strong>PRONTO! Bem-vindo ao VERBA VIVA!</strong></p>
-        <p class="text-justify">Você sabia que em 2024, a Prefeitura de Belo Horizonte destina R$73 milhões exclusivamente para obras do Orçamento Participativo (OP)? Sim, sua voz, seu poder!</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <h2 class="my-4">Mas como isso me beneficia?</h2>
-        <p class="text-justify">O OP te coloca no comando das decisões! Você escolhe quais melhorias serão feitas no seu município, garantindo que os recursos públicos sejam investidos em projetos que realmente impactam sua vida.</p>
-        <p class="text-justify">Cansado de ver o dinheiro público mal utilizado? O OP é a sua chance de mudar isso! Através dele, você pode:</p>
-        <ul class="list-group list-group-flush mb-4">
-          <li class="list-group-item">Propor projetos: Apresente ideias para obras e serviços que beneficiem sua comunidade.</li>
-          <li class="list-group-item">Votar nas prioridades: Decida quais projetos receberão os investimentos.</li>
-          <li class="list-group-item">Acompanhar a execução: Monitore de perto o andamento das obras e cobre resultados.</li>
-        </ul>
-        <p class="text-justify"><strong>Junte-se ao Verba Viva e faça a diferença!</strong></p>
-        <h3 class="my-4">O Verba Viva é um programa inovador que simplifica o OP:</h3>
-        <ul class="list-group list-group-flush mb-4">
-          <li class="list-group-item">Acesso facilitado: Utilize de uma interface simples e amigável para compreender aquilo que seria complicado.</li>
-          <li class="list-group-item">Informações na palma da mão: Tenha acesso a projetos de todas as regionais de BH e entenda para onde irão após propostos.</li>
-        </ul>
-        <p class="text-center"><strong>Seu bairro, suas decisões!</strong></p>
-        <p class="text-center"><strong>Não perca essa oportunidade! Participe do OP e ajude a construir uma Belo Horizonte mais justa, próspera e com a sua cara!</strong></p>
-      </div>
-    </div>
-  </div>
   </main>
 </template>
+
+
