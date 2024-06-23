b9621b4249f17ce9f0f08d978396dd435299b3d2
andradev101 <santos.andradetb@gmail.com>
2024-06-23 14:42:51 -0300

    admin view viewUser | edituser implemented

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <n-dialog-provider>
+      <n-notification-provider>
+        <n-message-provider>
+          <router-view></router-view>
+        </n-message-provider>
+      </n-notification-provider>
+    </n-dialog-provider>
   </div>
 </template>
 
 <script setup>
 import { user } from "./stateManagement/user.js";
+import { NMessageProvider, NNotificationProvider, NDialogProvider } from 'naive-ui'
 console.log("user.isSignedIn", user.isSignedIn);
 user.initialize();
 </script>
