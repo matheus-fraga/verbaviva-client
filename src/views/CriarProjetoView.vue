53ccd033b54d886694419ca02e5dc62515ee2538
GuiLanza <guilhermelanza1802@gmail.com>
2024-06-23 11:47:55 -0300

    Feitas as mudançans da tela de criação de projetos de Bootstrap para a Naive UI

--- src/views/CriarProjetoView.vue
+++ src/views/CriarProjetoView.vue
@@ -2,210 +2,222 @@
   <main>
     <NeoNavBar />
     <div class="container my-5">
-      <div class="row">
-        <div class="col-md-12">
+      <n-grid cols="1">
+        <n-grid-item>
           <h1 class="text-center">Criação de Projeto</h1>
-          <form @submit.prevent="createProjeto">
+          <n-form @submit.prevent="createProjeto" ref="formRef">
             <!-- Nome do Projeto -->
-            <div class="form-group mb-3">
-              <label for="nomeProjeto" class="form-label">Nome do Projeto</label>
-              <input type="text" class="form-control" id="nomeProjeto" placeholder="Nome do Projeto" v-model="projeto.nome" required>
-            </div>
+            <n-form-item label="Nome do Projeto" path="projeto.nome" :rule="rules.nome">
+              <n-input v-model="projeto.nome" placeholder="Nome do Projeto" required />
+            </n-form-item>
             
             <!-- Descrição do Projeto -->
-            <div class="form-group mb-3">
-              <label for="descricaoProjeto" class="form-label">Descrição do Projeto</label>
-              <textarea class="form-control" id="descricaoProjeto" placeholder="Descrição do Projeto" v-model="projeto.descricao" required></textarea>
-            </div>
-            
-            <!-- Rua -->
-            <div class="form-group mb-3">
-              <label for="rua" class="form-label">Rua</label>
-              <input type="text" class="form-control" id="rua" placeholder="Rua" v-model="endereco.rua" required>
-            </div>
-
+            <n-form-item label="Descrição do Projeto" path="projeto.descricao" :rule="rules.descricao">
+              <n-input type="textarea" v-model="projeto.descricao" placeholder="Descrição do Projeto" required />
+            </n-form-item>
             <!-- CEP -->
-            <div class="form-group mb-3">
-              <label for="cep" class="form-label">CEP</label>
-              <input type="text" class="form-control" id="cep" placeholder="CEP" v-model="endereco.cep" required>
-            </div>
+            <n-form-item label="CEP" path="endereco.cep" :rule="rules.cep">
+              <n-input v-model="endereco.cep" @blur="fetchEndereco" placeholder="CEP" required />
+            </n-form-item>
+
+            <!-- Rua -->
+            <n-form-item label="Rua" path="endereco.rua" :rule="rules.rua">
+              <n-input v-model="endereco.rua" placeholder="Rua" required />
+            </n-form-item>
 
             <!-- Bairro -->
-            <div class="form-group mb-3">
-              <label for="bairro" class="form-label">Bairro</label>
-              <input type="text" class="form-control" id="bairro" placeholder="Bairro" v-model="endereco.bairro" required>
-            </div>
+            <n-form-item label="Bairro" path="endereco.bairro" :rule="rules.bairro">
+              <n-input v-model="endereco.bairro" placeholder="Bairro" required />
+            </n-form-item>
 
             <!-- Município -->
-            <div class="form-group mb-3">
-              <label for="municipio" class="form-label">Município</label>
-              <input type="text" class="form-control" id="municipio" placeholder="Município" v-model="endereco.municipio" required>
-            </div>
+            <n-form-item label="Município" path="endereco.municipio" :rule="rules.municipio">
+              <n-input v-model="endereco.municipio" placeholder="Município" required />
+            </n-form-item>
 
             <!-- Estado -->
-            <div class="form-group mb-3">
-              <label for="estado" class="form-label">Estado</label>
-              <input type="text" class="form-control" id="estado" placeholder="Estado" v-model="endereco.estado" required>
-            </div>
+            <n-form-item label="Estado" path="endereco.estado" :rule="rules.estado">
+              <n-input v-model="endereco.estado" placeholder="Estado" required />
+            </n-form-item>
 
-<!--           
-            <div class="form-group mb-3">
-              <label for="coordenadaX" class="form-label">Coordenada X</label>
-              <input type="text" class="form-control" id="coordenadaX" placeholder="Coordenada X" v-model="endereco.coordenadaX" required>
-            </div>
-
-           
-            <div class="form-group mb-3">
-              <label for="coordenadaY" class="form-label">Coordenada Y</label>
-              <input type="text" class="form-control" id="coordenadaY" placeholder="Coordenada Y" v-model="endereco.coordenadaY" required>
-            </div> -->
+            <!-- Nome do Projeto -->
+            <n-form-item label="Nome do Projeto" path="projeto.nome" :rule="rules.nome">
+              <n-input v-model="projeto.nome" placeholder="Nome do Projeto" required />
+            </n-form-item>
             
+            <!-- Descrição do Projeto -->
+            <n-form-item label="Descrição do Projeto" path="projeto.descricao" :rule="rules.descricao">
+              <n-input type="textarea" v-model="projeto.descricao" placeholder="Descrição do Projeto" required />
+            </n-form-item>
+
             <!-- Botão de Submissão -->
-            <button type="submit" class="btn btn-primary w-100">Criar Projeto</button>
-          </form>
-        </div>
-      </div>
+            <n-button type="primary" class="w-100" native-type="submit">Criar Projeto</n-button>
+          </n-form>
+        </n-grid-item>
+      </n-grid>
     </div>
   </main>
 </template>
 
-<script>
-import NeoNavBar from '../components/NeoNavBar.vue'
-import { user } from '../stateManagement/user';
 
-export default {
+
+<script>
+import NeoNavBar from '../components/NeoNavBar.vue';
+import { user } from '../stateManagement/user';
+import { defineComponent } from 'vue';
+import {
+  NForm,
+  NFormItem,
+  NInput,
+  NButton,
+  NGrid,
+  NGridItem
+} from 'naive-ui';
+
+export default defineComponent({
   name: 'CriarProjetoView',
   components: {
-    NeoNavBar
+    NeoNavBar,
+    NForm,
+    NFormItem,
+    NInput,
+    NButton,
+    NGrid,
+    NGridItem
   },
   data() {
     return {
       projeto: {
         nome: '',
         descricao: '',
-        dataCriacao: new Date().toISOString().split('T')[0], // Data atual
+        dataCriacao: new Date().toISOString().split('T')[0],
         enderecoId: null,
-        usuarioId:  user.getUserData().data.id
+        usuarioId: user.getUserData().data.id
       },
       endereco: {
         rua: '',
         cep: '',
         bairro: '',
         municipio: '',
-        estado: '',
-        // coordenadaX: '',
-        // coordenadaY: ''
+        estado: ''
       },
-      errorMessage:'',
-      error:false
-      
-    }
+      errorMessage: '',
+      error: false,
+      rules: {
+        nome: {
+          required: true,
+          trigger: ['blur', 'input'],
+          message: 'O nome do projeto deve ter mais de uma letra e conter apenas letras.',
+          validator(rule, value) {
+            return value && value.length > 1 && /^[a-zA-Z\s]+$/.test(value);
+          }
+        },
+        descricao: {
+          required: true,
+          trigger: ['blur', 'input'],
+          message: 'A descrição do projeto deve ter mais de 10 caracteres.',
+          validator(rule, value) {
+            return value && value.length > 10;
+          }
+        },
+        rua: {
+          required: true,
+          trigger: ['blur', 'input'],
+          message: 'A rua deve ter mais de uma letra.',
+          validator(rule, value) {
+            return value && value.length > 1;
+          }
+        },
+        cep: {
+          required: true,
+          trigger: ['blur', 'input'],
+          message: 'O CEP deve estar no formato 00000-000.',
+          validator(rule, value) {
+            return value && /^\d{5}-\d{3}$/.test(value);
+          }
+        },
+        bairro: {
+          required: true,
+          trigger: ['blur', 'input'],
+          message: 'O bairro deve ter mais de uma letra.',
+          validator(rule, value) {
+            return value && value.length > 1;
+          }
+        },
+        municipio: {
+          required: true,
+          trigger: ['blur', 'input'],
+          message: 'O município deve ter mais de uma letra.',
+          validator(rule, value) {
+            return value && value.length > 1;
+          }
+        },
+        estado: {
+          required: true,
+          trigger: ['blur', 'input'],
+          message: 'O estado deve ter mais de uma letra.',
+          validator(rule, value) {
+            return value && value.length > 1;
+          }
+        }
+      }
+    };
   },
   methods: {
-
-    validateNome() {
-      if (this.projeto.nome.length <= 1) {
-        this.errorMessage = 'O nome do projeto deve ter mais de uma letra.';
-        this.error = true 
-      } else if (!/^[a-zA-Z\s]+$/.test(this.projeto.nome)) {
-        this.errorMessage = 'O nome do projeto deve conter apenas letras.';
-        this.error = true 
-      } else {
-        this.errorMessage = '';
-
-      }
-    },
-    validateDescricao() {
-      if (this.projeto.descricao.length <= 10) {
-        this.errorMessage  = 'A descrição do projeto deve ter mais de 10 caracteres.';
-        this.error = true 
-      } else {
-        this.errorMessage  = '';
-      }
-    },
-    validateDataCriacao() {
-      const currentDate = new Date().toISOString().split('T')[0];
-      if (this.projeto.dataCriacao < currentDate) {
-        this.errorMessage  = 'A data de criação deve ser a partir de hoje.';
-        this.error = true 
-      } else {
-        this.errorMessage  = '';
-      }
-    },
-    validateRua() {
-      if (this.endereco.rua.length <= 1) {
-        this.errorMessage  = 'A rua deve ter mais de uma letra.';
-        this.error = true 
-      } else {
-        this.errorMessage  = '';
-      }
-    },
-    validateCep() {
-      if (!/^\d{5}-\d{3}$/.test(this.endereco.cep)) {
-        this.errorMessage  = 'O CEP deve estar no formato 00000-000.';
-        this.error = true 
-      } else {
-        this.errorMessage  = '';
-      }
-    },
-    validateBairro() {
-      if (this.endereco.bairro.length <= 1) {
-        this.errorMessage  = 'O bairro deve ter mais de uma letra.';
-        this.error = true 
-      } else {
-        this.errorMessage  = '';
-      }
-    },
-    validateMunicipio() {
-      if (this.endereco.municipio.length <= 1) {
-        this.errorMessage  = 'O município deve ter mais de uma letra.';
-        this.error = true 
-      } else {
-        this.errorMessage  = '';
-      }
-    },
-    validateEstado() {
-      if (this.endereco.estado.length <= 1) {
-        this.errorMessage  = 'O estado deve ter mais de uma letra.';
-        this.error = true 
-      } else {
-        this.errorMessage  = '';
-      }
-    },
     validateForm() {
-      const validators = [
-        this.validateNome,
-        this.validateDescricao,
-        this.validateDataCriacao,
-        this.validateRua,
-        this.validateCep,
-        this.validateBairro,
-        this.validateMunicipio,
-        this.validateEstado
-      ];
+      const formRef = this.$refs.formRef;
+      return new Promise((resolve) => {
+        formRef.validate((errors) => {
+          if (errors) {
+            this.errorMessage = errors[0].message;
+            this.error = true;
+            alert(this.errorMessage);
+            resolve(false);
+          } else {
+            alert('Formulário válido!');
+            resolve(true);
+          }
+        });
+      });
+    },
+    async fetchEndereco() {
+      const cep = this.endereco.cep;
+      console.log('CEP inserido:', cep);
 
+      const cepFormatado = cep.replace(/\D/g, ''); // Remove qualquer caractere não numérico
+      console.log('CEP formatado:', cepFormatado);
 
+      if (!cepFormatado || cepFormatado.length !== 8) {
+        alert('CEP inválido. O formato deve ser 00000-000.');
+        return;
+      }
 
-      // Executa cada função de validação
-      validators.forEach((validator) => {
-          if (!this.error)
-            validator()
-          }); 
+      try {
+        const response = await fetch(`https://viacep.com.br/ws/${cepFormatado}/json/`);
+        if (!response.ok) {
+          throw new Error('Erro ao buscar o endereço.');
+        }
 
-      if (
-        !this.error
-      ) {
-        alert('Formulário válido!');
-        return true 
-      } else {
-        alert(this.errorMessage );
-        return false
+        const data = await response.json();
+        if (data.erro) {
+          throw new Error('CEP não encontrado.');
+        }
+
+        console.log('Dados recebidos do ViaCEP:', data);
+        this.endereco.rua = data.logradouro;
+        this.endereco.bairro = data.bairro;
+        this.endereco.municipio = data.localidade;
+        this.endereco.estado = data.uf;
+      } catch (error) {
+        alert(error.message);
+        console.error('Erro ao buscar o endereço:', error);
       }
     },
-    createProjeto() {
-      if(!this.validateForm()){
-        this.error = false
-        return
+    async createProjeto() {
+      const isValid = await this.validateForm();
+      if (!isValid) {
+        this.error = false;
+        return;
       }
       // Primeiro, criar o endereço
       fetch('http://localhost:8080/enderecos', {
@@ -215,35 +227,33 @@ export default {
         },
         body: JSON.stringify(this.endereco)
       })
-      .then(response => response.json())
-      .then(enderecoData => {
-        console.log('Endereço criado:', enderecoData);
-        this.projeto.enderecoId = enderecoData.id;
+        .then(response => response.json())
+        .then(enderecoData => {
+          console.log('Endereço criado:', enderecoData);
+          this.projeto.enderecoId = enderecoData.id;
 
-        // Em seguida, criar o projeto com o endereçoId
-        return fetch('http://localhost:8080/projetos', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(this.projeto)
+          // Em seguida, criar o projeto com o enderecoId
+          return fetch('http://localhost:8080/projetos', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.projeto)
+          });
+        })
+        .then(response => response.json())
+        .then(projetoData => {
+          console.log('Projeto criado:', projetoData);
+          this.$router.push('/');
+        })
+        .catch(error => {
+          console.error('Erro ao criar o projeto:', error);
         });
-      })
-      .then(response => response.json())
-      .then(projetoData => {
-        console.log('Projeto criado:', projetoData);
-        this.$router.push("/");
-      })
-      .catch(error => {
-        console.error("Erro ao criar o projeto:", error);
-      });
     }
   }
-
-} 
-  
-
+});
 </script>
+
 
 <style scoped>
 /* Adicione qualquer estilo específico para esta view aqui */
